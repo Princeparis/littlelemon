60f50e6d3d8bd0c192175e03c097114d79ec079f
Yusuff Ridwan <wemzain@gmail.com>
2023-02-23 00:18:35 +0100

    added rest_framework authentication and djoser plus some minor changes

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
-from django.http import request
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+from .models import Booking, Menu
+from .serializers import BookingSerializer, MenuSerializer
 
 # Create your views here.
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+class MenuView(ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class SingleMenuView(RetrieveUpdateAPIView, DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class BookingViewSet(ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
